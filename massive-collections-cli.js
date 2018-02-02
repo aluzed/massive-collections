10965f9279d02b1ed76e172e14511e7451fd2d3f
Alexandre PENOMBRE <alexandre.penombre+Github@gmail.com>
2018-02-02 14:07:28 +0100

    clean help

--- massive-collections-cli.js
+++ massive-collections-cli.js
@@ -281,50 +281,8 @@ try {
       break;
 
       case 'help':
-        log.info("\n====================================\n");
-        log.info("\n| How To Use Massive-Collection CLI |\n");
-        log.info("\n====================================\n");
-        log.warn("\nAvailable commands : \n");
-        log.info(`
-  * connect                     Connect to Database (permanent)
-  * disconnect                  Disconnect
-  * create-table | createTable  Create a table
-          `);
-        log.warn("\nParameters : \n");
-        log.info(`
-  * connect [parameters]
-
-  list :
-  --h     host:port
-  --db    database
-  --p     password
-  --u     user
-
-  Example : massive-collections-cli connect --h=localhost:5432 --db=test_db --u=root --p=root
-  `);
-
-        log.info(`
-  * createTable [tableName] [columns]
-
-  column format:
-  name:type:[index]:[nullable]:[default]
-
-  * Details :
-  name      Any
-  type      Any postgresql type (or shortcuts : timestampz, int, bool)
-  index     <unique|noindex> (optionnal)
-  nullable  <null|notnull> (optionnal)
-  default   <now()|true|false|...> (optionnal)
-
-  Example :
-  massive-collections createTable users \\
-  username:varchar(255):unique:notnull \\
-  password:varchar(255):noindex:notnull \\
-  age:int \\
-  details:jsonb \\
-  created:timestampz:noindex:null:now() \\
-  modified:timestampz:noindex:null:now()
-  `)
+        // Cleaner
+        require('./help');
       break;
     }
   }
